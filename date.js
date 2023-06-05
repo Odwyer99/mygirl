0a264421b3ac2a8d7dbbe13fd2022d0eaa8fa208
Odwyer99 <charlynoni79@gmail.com>
2023-06-05 16:00:49 +0100

    first commit

---
+++ date.js
@@ -0,0 +1,9 @@
+function handleYes() {
+    alert("Congratulations on making this decision baby. I love you!");
+    // Additional actions or redirect to another page if desired
+}
+
+function handleNo() {
+    alert("Werey dey find No lmaooo Mo gbe won ni handicap hahahaha");
+    // Additional actions or redirect to another page if desired
+}
